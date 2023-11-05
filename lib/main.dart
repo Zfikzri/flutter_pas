c0a3e861fecefdb3d74b5cf6a2cbe3f92329324e
Zfikzri <zfikzri07@gmail.com>
2023-11-05 07:57:13 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_pas/pages/splash.dart';
+import 'package:get/get_navigation/get_navigation.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        backgroundColor: const Color(0xFFF8E8EE),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: SplashScreen(),
+    );
+  }
+}
---
+++ lib/pages/splash.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_pas/pages/home.dart';
+import 'package:get/get.dart';
+
+class SplashScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Future.delayed(
+      Duration(seconds: 5), 
+      () {
+        Get.offAll(() => Home()); 
+      },
+    );
+
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              "aeria", 
+              style: TextStyle(
+                fontFamily: "Gloria Ramsey",
+                fontSize: 115,
+                color: Color(0xFFA52B57),  
+                 height: 0.10,
+              ),
+         
+            ),
+            Text(
+              "everyone is beauty", 
+              style: TextStyle(
+                fontFamily: "ProductSans",
+                fontSize: 19,
+                color: Color(0xFFC75E84), 
+               
+              ),
+        
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
